df13b07833ec40075ee3a24fff8c31138213c986
ygrek <ygreksh@gmail.com>
2021-08-17 17:10:44 +0300

    sample1

---
+++ BogusSample/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace BogusSample
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string PassportNumber { get; set; }
+    }
+}
---
+++ BogusSample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Bogus;
+
+namespace BogusSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var faker = new Faker("en");
+            var person = new Person()
+            {
+                Name = faker.Name.FullName(),
+                Age = faker.Random.Number(18,120),
+                PassportNumber = faker.Random.String(10,10,'\0','\uffff')
+            };
+            Console.WriteLine($"{person.PassportNumber}, {person.Name}, {person.Age}");
+        }
+    }
+}
