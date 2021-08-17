642feb6a86b44fc8c4b8201b6d497f915d7f6366
ygrek <ygreksh@gmail.com>
2021-08-17 17:27:44 +0300

    sample2

--- BogusSample/Program.cs
+++ BogusSample/Program.cs
@@ -12,7 +12,8 @@ namespace BogusSample
             {
                 Name = faker.Name.FullName(),
                 Age = faker.Random.Number(18,120),
-                PassportNumber = faker.Random.String(10,10,'\0','\uffff')
+                //PassportNumber = faker.Random.String(10,10,'\0','\uffff')
+                PassportNumber = faker.Random.String2(8, "qwertyuiopasdfghjklzxcvbnm")
             };
             Console.WriteLine($"{person.PassportNumber}, {person.Name}, {person.Age}");
         }
