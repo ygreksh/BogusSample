42181db2ed2ed384817ed9a323b3d81144611645
ygrek <ygreksh@gmail.com>
2021-08-19 17:09:21 +0300

    influence sample

--- BogusSample/Program.cs
+++ BogusSample/Program.cs
@@ -7,15 +7,23 @@ namespace BogusSample
     {
         static void Main(string[] args)
         {
-            var faker = new Faker("en");
+            var faker = new Faker("ru");
             var person = new Person()
             {
                 Name = faker.Name.FullName(),
                 Age = faker.Random.Number(18,100),
-                //PassportNumber = faker.Random.String(10,10,'\0','\uffff')
-                PassportNumber = faker.Random.String2(2, "QWERTYUIOPLKJHGFDSAZXCVBNM") + "-" + faker.Random.String2(8, "0123456789")
+                PassportNumber = faker.Random.String2(2, "ABCDEFGHIJKLMNOPQRSTUVWXYZ") + "-" + faker.Random.String2(8, "0123456789")
             };
             Console.WriteLine($"{person.PassportNumber}, {person.Name}, {person.Age}");
+
+            var testPersons = new Faker<Person>()
+                .RuleFor(p => p.Name, f => f.Name.FullName())
+                .RuleFor(p => p.Age, f => f.Random.Number(18, 100))
+                .RuleFor(p => p.PassportNumber,
+                    f => f.Random.String2(2, "ABCDEFGHIJKLMNOPQRSTUVWXYZ") + "-" +
+                         faker.Random.String2(8, "0123456789"));
+            var person2 = testPersons.Generate();
+            Console.WriteLine($"{person2.PassportNumber}, {person2.Name}, {person2.Age}");
         }
     }
 }
